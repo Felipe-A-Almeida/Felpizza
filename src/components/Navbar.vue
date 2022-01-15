dfa4a6fc82b9ddc355aac396990ac395a0870568
Felpson <felipe_a.almeida@outlook.com>
2022-01-15 19:48:36 -0300

    insert Navbar and Footer files and removes unecessary files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+   <div id="nav">
+     <router-link to="/">
+      <img src="" alt="" id="logo">
+     </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/orders">Orders</router-link>
+  </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default {
+  name: "Navbar"
+}
+</script>
+<style scoped>
+
+</style>
